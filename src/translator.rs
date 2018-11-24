603310f58e73bf73fb823524c56898fd23a47fbf
Denis Defreyne <denis.defreyne@stoneship.org>
2018-11-24 12:28:24 +0100

    Let Translator deal with nodes, not vectors of nodes

--- src/translator.rs
+++ src/translator.rs
@@ -1,19 +1,15 @@
 use super::{ElementNode, Node, StringNode};
 
 pub trait Translator {
-    fn translate(&self, nodes: Vec<Node>) -> String {
-        let mut res = String::new();
-        for node in nodes {
-            res.push_str(&match node {
-                Node::Element(n) => self.translate_element(n),
-                Node::String(n) => self.translate_string(n),
-            })
+    fn translate(&self, node: &Node) -> String {
+        match node {
+            Node::Element(n) => self.translate_element(n),
+            Node::String(n) => self.translate_string(n),
         }
-        res
     }
 
-    fn translate_element(&self, node: ElementNode) -> String;
-    fn translate_string(&self, node: StringNode) -> String;
+    fn translate_element(&self, node: &ElementNode) -> String;
+    fn translate_string(&self, node: &StringNode) -> String;
 }
 
 #[cfg(test)]
@@ -25,16 +21,20 @@ mod tests {
     struct SimpleTranslator {}
 
     impl Translator for SimpleTranslator {
-        fn translate_element(&self, node: ElementNode) -> String {
+        fn translate_element(&self, node: &ElementNode) -> String {
             format!(
                 "elem(name={:?}, attrs={:?}, children=[{}])",
                 node.name,
                 node.attributes,
-                self.translate(node.children),
+                node.children
+                    .iter()
+                    .map(|c| self.translate(c))
+                    .collect::<Vec<String>>()
+                    .join(","),
             )
         }
 
-        fn translate_string(&self, node: StringNode) -> String {
+        fn translate_string(&self, node: &StringNode) -> String {
             format!("str({:?})", node.content)
         }
     }
@@ -44,21 +44,17 @@ mod tests {
         let mut attrs = HashMap::new();
         attrs.insert("foo".into(), "bar".into());
 
-        let input = vec![
-            Node::Element(ElementNode {
-                name: "root-elem".into(),
-                attributes: attrs,
-                children: vec![Node::String(StringNode {
-                    content: "child-str".into(),
-                })],
-            }),
-            Node::String(StringNode {
-                content: "root-str".into(),
-            }),
-        ];
+        let input = Node::Element(ElementNode {
+            name: "root-elem".into(),
+            attributes: attrs,
+            children: vec![Node::String(StringNode {
+                content: "child-str".into(),
+            })],
+        });
         assert_eq!(
-            SimpleTranslator {}.translate(input),
-            "elem(name=\"root-elem\", attrs={\"foo\": \"bar\"}, children=[str(\"child-str\")])str(\"root-str\")".to_string()
+            SimpleTranslator {}.translate(&input),
+            "elem(name=\"root-elem\", attrs={\"foo\": \"bar\"}, children=[str(\"child-str\")])"
+                .to_string()
         );
     }
 }
