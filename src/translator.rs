458ad51b195b8c58946c02303f5fec5c64dac385
Denis Defreyne <denis.defreyne@stoneship.org>
2018-11-24 12:35:46 +0100

    Make Translator generic (not just strings)

--- src/translator.rs
+++ src/translator.rs
@@ -1,15 +1,15 @@
 use super::{ElementNode, Node, StringNode};
 
-pub trait Translator {
-    fn translate(&self, node: &Node) -> String {
+pub trait Translator<T> {
+    fn translate(&self, node: &Node) -> T {
         match node {
             Node::Element(n) => self.translate_element(n),
             Node::String(n) => self.translate_string(n),
         }
     }
 
-    fn translate_element(&self, node: &ElementNode) -> String;
-    fn translate_string(&self, node: &StringNode) -> String;
+    fn translate_element(&self, node: &ElementNode) -> T;
+    fn translate_string(&self, node: &StringNode) -> T;
 }
 
 #[cfg(test)]
@@ -18,9 +18,9 @@ mod tests {
     use super::{ElementNode, Node, StringNode};
     use std::collections::HashMap;
 
-    struct SimpleTranslator {}
+    struct SampleStringTranslator {}
 
-    impl Translator for SimpleTranslator {
+    impl Translator<String> for SampleStringTranslator {
         fn translate_element(&self, node: &ElementNode) -> String {
             format!(
                 "elem(name={:?}, attrs={:?}, children=[{}])",
@@ -39,8 +39,29 @@ mod tests {
         }
     }
 
+    #[derive(PartialEq, Debug)]
+    enum TreeNode {
+        Elem(String, Vec<TreeNode>),
+        Str(String),
+    }
+
+    struct SampleTreeTranslator {}
+
+    impl Translator<TreeNode> for SampleTreeTranslator {
+        fn translate_element(&self, node: &ElementNode) -> TreeNode {
+            TreeNode::Elem(
+                node.name.to_owned().to_string(),
+                node.children.iter().map(|c| self.translate(c)).collect(),
+            )
+        }
+
+        fn translate_string(&self, node: &StringNode) -> TreeNode {
+            TreeNode::Str(node.content.to_owned().to_string())
+        }
+    }
+
     #[test]
-    fn simple() {
+    fn example_string() {
         let mut attrs = HashMap::new();
         attrs.insert("foo".into(), "bar".into());
 
@@ -52,9 +73,30 @@ mod tests {
             })],
         });
         assert_eq!(
-            SimpleTranslator {}.translate(&input),
+            SampleStringTranslator {}.translate(&input),
             "elem(name=\"root-elem\", attrs={\"foo\": \"bar\"}, children=[str(\"child-str\")])"
                 .to_string()
+        );
+    }
+
+    #[test]
+    fn example_tree() {
+        let mut attrs = HashMap::new();
+        attrs.insert("foo".into(), "bar".into());
+
+        let input = Node::Element(ElementNode {
+            name: "root-elem".into(),
+            attributes: attrs,
+            children: vec![Node::String(StringNode {
+                content: "child-str".into(),
+            })],
+        });
+        assert_eq!(
+            SampleTreeTranslator {}.translate(&input),
+            TreeNode::Elem(
+                "root-elem".to_string(),
+                vec![TreeNode::Str("child-str".into())]
+            )
         );
     }
 }
